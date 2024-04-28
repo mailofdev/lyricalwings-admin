30e005dfd1fc89fc2f2ebdb08e3cbc9c0c47dd5c
patilank <ankit.patil@ikione.com>
2024-04-28 19:54:25 +0530

    new project added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MainScreen from './Screens/MainScreen';
-import Login from './Screens/Login';
 
 function App() {
-
-  const [isAuth, setIsAuth] = useState(false);
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login setIsAuth={setIsAuth} />} />
-        <Route path="/MainScreen" element={<MainScreen />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
