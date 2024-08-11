cdf658902370951e04aee3ae4517fa0f4ec6f246
patilank <ankit.patil@ikione.com>
2024-08-11 08:38:08 +0530

    private routing

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PoemForm from "./Screens/PoemForm";
-import PoemList from './Screens/PoemList';
 import Dashboard from "./Screens/Dashboard";
 import Header from './Screens/Header';
 import AuthModal from './Components/AuthModal';
 import About from './Screens/About';
-import DetailPoem from './Screens/DetailPoem';
 import Courses from './Screens/Courses';
 import User from './Screens/User';
 import StoryAndNovel from './Screens/StoryAndNovel';
-import StoryAndNovelList from './Screens/StoryAndNovelList'
-import DetailStoryAndNovels from './Screens/DetailStoryAndNovels';
-import UI from './Screens/UI';
 import ItemList from './Screens/ItemList';
 import ItemDetails from './Screens/ItemDetails';
+import PrivateRoute from './Components/PrivateRoute'; // Import the PrivateRoute component
 
 function App() {
   return (
@@ -24,18 +20,19 @@ function App() {
       <Header />
       <div className='pt-40 mt-40'>
         <Routes>
-        <Route path="/" element={<AuthModal show={true} handleClose={() => {}} />} />
-        <Route path="*" element={<AuthModal show={true} handleClose={() => {}} />} />
+          {/* Public Routes */}
+          <Route path="/" element={<AuthModal show={true} handleClose={() => {}} />} />
+          <Route path="*" element={<AuthModal show={true} handleClose={() => {}} />} />
 
-          <Route path="/Dashboard/*" element={<Dashboard />} />
-          <Route path="/PoemForm" element={<PoemForm />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Courses" element={<Courses />} />
-          <Route path="/User" element={<User />} />
-          <Route path="/StoryAndNovel" element={<StoryAndNovel />} />
-          <Route path="/ItemList/:type" element={<ItemList />} /> 
-          <Route path="/item/:id" element={<ItemDetails />} />
-
+          {/* Private Routes */}
+          <Route path="/Dashboard/*" element={<PrivateRoute element={Dashboard} />} />
+          <Route path="/PoemForm" element={<PrivateRoute element={PoemForm} />} />
+          <Route path="/About" element={<PrivateRoute element={About} />} />
+          <Route path="/Courses" element={<PrivateRoute element={Courses} />} />
+          <Route path="/User" element={<PrivateRoute element={User} />} />
+          <Route path="/StoryAndNovel" element={<PrivateRoute element={StoryAndNovel} />} />
+          <Route path="/ItemList/:type" element={<PrivateRoute element={ItemList} />} />
+          <Route path="/item/:id" element={<PrivateRoute element={ItemDetails} />} />
         </Routes>
       </div>
     </div>
